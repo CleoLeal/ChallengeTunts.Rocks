03620098efb054ba2550b74fe84f1325fea0c047
Cleo Leal <94088457+CleoLeal@users.noreply.github.com>
2024-01-30 10:36:16 -0300

    Update framework
    Updated the console view and function creation for Clear console.

--- framework.py
+++ framework.py
@@ -2,15 +2,21 @@
 import pandas as pd #data manipulation and analysis library 
 import numpy as np # data control
 import math # librarie math to rounded
+import platform 
+import os
 
 #caculating the avarage
 def Average (n1, n2, n3) -> int:
     # calculating the average and ignore values NaN
     a = (n1 + n2 + n3) / 3
-
     # Rounded for up the average values
     rounded_avarage = a.apply(lambda x: np.ceil(x) if not pd.isna(x) else x)
-    print(f'Avarege:{rounded_avarage}')
+    
+     # Print only once
+    if not hasattr(Average, 'printed'):
+        print(f'=== Average calculated ===')
+        Average.printed = True
+    
     return rounded_avarage
 
 #calculating schools absences
@@ -23,5 +29,17 @@ def SchoolsAbsences(f) -> bool:
 def Naf(m) -> int:
     naf = math.ceil((50 - m) * 2)  # formula
     condition = math.ceil((m + naf)/2)
-    print(f"Grade for final approval {condition}") 
+
+    # Print only once
+    if not hasattr(Naf, 'printed'):
+        print(f" === Naf calculated === ") 
+        Naf.printed = True
     return condition
+
+def Clear():
+    if platform.system() == "Windows":#plataform windows
+        #command windows
+        os.system("cls")
+    elif platform.system() == "Darwin" or platform.system() == "Linux":
+        #commanda macos and linux
+        os.system("clear")
